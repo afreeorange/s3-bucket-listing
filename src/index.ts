b13391e2a07337e66d5e10aa60b0f76c7f6e9fdd
Nikhil Anand <nikhilanand@granular.ag>
2021-07-18 13:18:45 -0500

    Initial prefix fix

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import { DEFAULT_BUCKET, DEFAULT_PREFIX } from "./constants";
 
 (async () => {
   const bucket = process.env.BUCKET || DEFAULT_BUCKET;
+  const prefix = process.env.PREFIX;
 
-  window.onload = () =>
-    drawDOM(bucket, location.hash.substring(1).replace(/%20/g, " "));
+  const initialPrefix = prefix
+    ? prefix
+    : location.hash.substring(1).replace(/%20/g, " ");
+
+  window.onload = () => drawDOM(bucket, initialPrefix);
 
   window.onhashchange = () =>
     drawDOM(bucket, location.hash.substring(1).replace(/%20/g, " "));
