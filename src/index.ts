c92f197de7841d9741284163c2a5c2bf7616cee4
Nikhil Anand <nikhilanand@granular.ag>
2021-07-18 13:05:01 -0500

    Bug?

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import { drawDOM } from "./dom";
 import { DEFAULT_BUCKET, DEFAULT_PREFIX } from "./constants";
-import { BucketConfiguration } from "./types";
 
 (async () => {
   const bucket = process.env.BUCKET || DEFAULT_BUCKET;
-  const prefix = process.env.PREFIX || DEFAULT_PREFIX;
+  // const prefix = process.env.PREFIX || DEFAULT_PREFIX;
 
   window.onload = () =>
-    drawDOM(bucket, prefix);
+    drawDOM(bucket, location.hash.substring(1).replace(/%20/g, " "));
 
   window.onhashchange = () =>
     drawDOM(bucket, location.hash.substring(1).replace(/%20/g, " "));
